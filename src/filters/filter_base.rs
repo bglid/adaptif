5639762e673c29d8704d3cd5b9d69d7c47b42e3e
elnaske <naske.elias@outlook.com>
2026-09-11 16:20:01 +0200

    Move common filter logic to filters/common.rs

--- src/filters/filter_base.rs
+++ src/filters/filter_base.rs
@@ -1,10 +1,10 @@
 use crate::algorithms::Algorithm;
-use crate::error::{Error, Result};
+use crate::error::Result;
 use crate::types::buffers::NoiseBuffer;
-use crate::types::signals::{
-    InputSample, InputSignal, NoiseReference, NoiseSample, OutputSample, OutputSignal,
-};
-use crate::types::{FilterWeights, NoiseEstimate, WindowSize};
+use crate::types::signals::{InputSignal, NoiseReference, OutputSignal};
+use crate::types::{FilterWeights, WindowSize};
+
+use crate::filters::common::{check_signal_lengths, process_sample};
 
 // TODO: make f64 generic
 
@@ -20,7 +20,7 @@ pub struct FilterBase<A: Algorithm> {
 }
 impl<A: Algorithm> FilterBase<A> {
     /// Initializes a filter using the provided algorithm configuration and window size.
-    /// The weights are samples from a normal distribution with $\mu = 0.0 and $\sigma$ = 5e-5.
+    /// The weights are intialized to zero.
     pub fn new(algorithm: A, window_size: usize) -> Option<Self> {
         let window_size = WindowSize::new(window_size)?;
 
@@ -117,45 +117,14 @@ impl<A: Algorithm> FilterBase<A> {
     }
 }
 
-fn process_sample(
-    weights: &FilterWeights,
-    noise_ref_buffer: &mut NoiseBuffer,
-    input_sample: InputSample,
-    noise_sample: NoiseSample,
-) -> OutputSample {
-    noise_ref_buffer.push(*noise_sample);
-
-    let noise_estimate = estimate_noise(weights, noise_ref_buffer);
-
-    compute_error(input_sample, noise_estimate)
-}
-
-fn estimate_noise(weights: &FilterWeights, x_n: &NoiseBuffer) -> NoiseEstimate {
-    // NoiseBuffer is initiated with the same length as weights, therefore we don't need to check
-    NoiseEstimate(weights.iter().zip(x_n.iter()).map(|(w, x)| w * x).sum())
-}
-
-fn compute_error(input_sample: InputSample, noise_estimate: NoiseEstimate) -> OutputSample {
-    OutputSample(*input_sample - *noise_estimate)
-}
-
-fn check_signal_lengths(input_signal: &InputSignal, noise_ref: &NoiseReference) -> Result<()> {
-    if noise_ref.len() < input_signal.len() {
-        Err(Error::NoiseRefTooShort {
-            input_len: input_signal.len(),
-            noise_len: noise_ref.len(),
-        })
-    } else {
-        Ok(())
-    }
-}
-
 #[cfg(test)]
 #[allow(clippy::unwrap_used, clippy::indexing_slicing, reason = "Tests")]
 mod tests {
     use super::*;
 
-    use crate::test_utils::{all_approx_equal, approx_equal, noise_buffer_from};
+    use crate::error::Error;
+    use crate::test_utils::all_approx_equal;
+    use crate::types::signals::OutputSample;
 
     struct TestAlgorithm;
     impl Algorithm for TestAlgorithm {
@@ -180,25 +149,6 @@ mod tests {
             filter.weights[i] = *val;
         }
         filter
-    }
-
-    #[test]
-    fn estimate_noise_works() {
-        let filter = testing_filter();
-
-        let x_n = noise_buffer_from(&[2.0, 3.0, 4.0]);
-
-        let res = estimate_noise(&filter.weights, &x_n);
-        assert!(approx_equal(*res, -2.0, 1e-6));
-    }
-
-    #[test]
-    fn compute_error_works() {
-        assert!(approx_equal(
-            *compute_error(InputSample(5.0), NoiseEstimate(3.5)),
-            1.5,
-            1e-6
-        ));
     }
 
     #[test]
--- src/filters/mod.rs
+++ src/filters/mod.rs
@@ -1,6 +1,8 @@
 mod filter_base;
 pub use filter_base::FilterBase;
 
+mod common;
+
 use crate::algorithms::LeastMeanSquares;
 
 // Define aliases for easier use
