af3c6950e2c72cca51a61304252ce2860489ab6a
elnaske <naske.elias@outlook.com>
2026-09-12 11:07:56 +0200

    Alternate block adaptation w/o padding

--- src/filters/block_filter_base.rs
+++ src/filters/block_filter_base.rs
@@ -1,9 +1,11 @@
+use std::ops::Range;
+
 use crate::algorithms::BlockAlgorithm;
 
 use crate::error::Result;
 use crate::filters::common::{check_signal_lengths, compute_error};
 use crate::types::buffers::{BlockNoiseBuffer, ErrorBuffer};
-use crate::types::signals::{InputSample, InputSignal, NoiseReference, NoiseSample, OutputSignal};
+use crate::types::signals::{InputSignal, NoiseReference, OutputSignal};
 use crate::types::{BlockSize, FilterWeights, NoiseEstimate, WindowSize};
 
 pub struct BlockFilterBase<B: BlockAlgorithm> {
@@ -55,32 +57,68 @@ impl<B: BlockAlgorithm> BlockFilterBase<B> {
         let mut block_error = ErrorBuffer::new(self.block_size);
         let mut cleaned_signal = OutputSignal::new(input_signal);
 
-        for block_start in (0..input_signal.len()).step_by(*self.block_size) {
-            for n in block_start..block_start + *self.block_size {
-                // padding with 0.0 if necessary
-                let input_sample = input_signal.get_sample(n).unwrap_or(InputSample(0.0));
-                let noise_sample = noise_ref.get_sample(n).unwrap_or(NoiseSample(0.0));
+        let n_samples = input_signal.len();
 
-                noise_ref_buffer.push(*noise_sample);
+        for block_start in (0..n_samples).step_by(*self.block_size) {
+            let block_end = block_start + *self.block_size;
 
-                let current_window = noise_ref_buffer.iter().take(*self.window_size);
-                let noise_estimate = NoiseEstimate(
-                    self.weights
-                        .iter()
-                        .zip(current_window)
-                        .map(|(w, x)| w * x)
-                        .sum(),
+            if block_end < n_samples {
+                let _ = self.process_block(
+                    block_start..block_end,
+                    input_signal,
+                    noise_ref,
+                    &mut noise_ref_buffer,
+                    &mut block_error,
+                    &mut cleaned_signal,
                 );
 
-                let error = compute_error(input_sample, noise_estimate);
-                block_error.push(error);
-                cleaned_signal.push(error);
+                self.algorithm
+                    .update_block(&mut self.weights, &block_error, &noise_ref_buffer);
+            } else {
+                // last block: finish of remaining samples w/o updating the weights
+                let _ = self.process_block(
+                    block_start..n_samples,
+                    input_signal,
+                    noise_ref,
+                    &mut noise_ref_buffer,
+                    &mut block_error,
+                    &mut cleaned_signal,
+                );
             }
-
-            self.algorithm
-                .update_block(&mut self.weights, &block_error, &noise_ref_buffer);
         }
 
         Ok(cleaned_signal.into_inner())
+    }
+
+    fn process_block(
+        &self,
+        range: Range<usize>,
+        input_signal: &InputSignal,
+        noise_ref: &NoiseReference,
+        noise_ref_buffer: &mut BlockNoiseBuffer,
+        block_error: &mut ErrorBuffer,
+        cleaned_signal: &mut OutputSignal,
+    ) -> Option<()> {
+        for n in range {
+            let input_sample = input_signal.get_sample(n)?;
+            let noise_sample = noise_ref.get_sample(n)?;
+
+            noise_ref_buffer.push(*noise_sample);
+
+            let current_window = noise_ref_buffer.iter().take(*self.window_size);
+            let noise_estimate = NoiseEstimate(
+                self.weights
+                    .iter()
+                    .zip(current_window)
+                    .map(|(w, x)| w * x)
+                    .sum(),
+            );
+
+            let error = compute_error(input_sample, noise_estimate);
+            block_error.push(error);
+            cleaned_signal.push(error);
+        }
+
+        Some(())
     }
 }
