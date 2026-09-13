645d7a4b9dcb99a58efcdd9eb284e35966bf9977
bglid <benjamin.h.glidden@gmail.com>
2026-09-13 19:14:03 -0400

    RLS alias

--- src/filters/mod.rs
+++ src/filters/mod.rs
@@ -1,8 +1,9 @@
 mod filter_base;
 pub use filter_base::FilterBase;
 
-use crate::algorithms::{LeastMeanSquares, NormalizedLeastMeanSquares};
+use crate::algorithms::{LeastMeanSquares, NormalizedLeastMeanSquares, RecursiveLeastSquares};
 
 // Define aliases for easier use
 pub type LMSFilter = FilterBase<LeastMeanSquares>;
 pub type NLMSFilter = FilterBase<NormalizedLeastMeanSquares>;
+pub type RLSFilter = FilterBase<RecursiveLeastSquares>;
